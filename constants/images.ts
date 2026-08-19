35e78633dbcb6c4eaa197c7af884ee9393aa0512
Aditya Singh <adityasinghrajawat2004@gmail.com>
2026-08-19 09:38:53 +0530

    Read static images

--- constants/images.ts
+++ constants/images.ts
@@ -1,6 +1,4 @@
-// @ts-expect-error PNG asset modules are provided by the bundler.
 import splashPattern from '@/assets/images/splash-pattern.png'
-// @ts-expect-error PNG asset modules are provided by the bundler.
 import avatar from '@/assets/images/avatar.png'
 
 export default { splashPattern, avatar }
